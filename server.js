7250dd8bbbd9fbbc7c971c1e9c6dfc8408ca0ec6
Nanyte25 <freer.mark@gmail.com>
2021-04-05 12:13:25 +0100

    setting up api structure and database schema for application

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/posts', require('./routes/posts'));
 
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
