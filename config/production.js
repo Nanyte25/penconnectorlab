7c6571e8df5b76013dd5988c600302fed4467843
Nanyte25 <freer.mark@gmail.com>
2021-04-23 13:57:40 +0100

    make it better

--- config/production.js
+++
@@ -1,5 +0,0 @@
-{
-    "mongoURI": "mongodb+srv://User3:Tolkein_34@cluster0.h4hmc.mongodb.net/test?retryWrites=true&w=majority",
-    "jwtSecret": "secret",
-    "githubToken": "d58b23f70a184864c0d1b3935fc88ed16abca86c"
-  }
